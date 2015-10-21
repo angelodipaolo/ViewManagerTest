5c69c4d601140d5b3924b4e7fbae495c6ccae6ad
Angelo Di Paolo <angelod101@gmail.com>
2015-10-21 11:39:09 -0400

    initial commit

---
+++ ios/FooViewManager.h
@@ -0,0 +1,14 @@
+//
+//  FooViewManager.h
+//  ViewManagerTest
+//
+//  Created by Angelo Di Paolo on 10/21/15.
+//  Copyright © 2015 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "RCTViewManager.h"
+
+@interface FooViewManager : RCTViewManager
+
+@end
---
+++ ios/FooViewManager.m
@@ -0,0 +1,16 @@
+//
+//  FooViewManager.m
+//  ViewManagerTest
+//
+//  Created by Angelo Di Paolo on 10/21/15.
+//  Copyright © 2015 Facebook. All rights reserved.
+//
+
+#import "FooViewManager.h"
+#import "RCTViewManager.h"
+
+@implementation FooViewManager
+
+RCT_EXPORT_MODULE()
+
+@end
